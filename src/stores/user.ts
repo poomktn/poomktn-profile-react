0b24befd3ab7b55bfe7c05857ba2ef8743b5e291
Kantanat Siripipatworakun <phantomktn@gmail.com>
2023-02-17 10:50:51 +0700

    remove unused file

--- src/stores/user.ts
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import stxReducer from './indexRedux'
-// export const store = configureStore({
-//   reducer: {
-//     stx: stxReducer
-//   }
-// })
-export {}
